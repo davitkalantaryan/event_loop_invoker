69fd629ef50819ce6d74c040ec57f877578aea8c
Davit Kalantaryan <davit.kalantaryan@desy.de>
2024-12-29 17:39:25 +0100

    changed interface for simple event loop invoker

--- include/event_loop_invoker/event_loop_invoker.h
+++ include/event_loop_invoker/event_loop_invoker.h
@@ -23,17 +23,25 @@ typedef void* (*EvLoopInvokerBlockedClbk)(struct EvLoopInvokerHandle*CPPUTILS_AR
 typedef void (*EvLoopInvokerAsyncClbk)(struct EvLoopInvokerHandle* CPPUTILS_ARG_NN,void*);
 typedef bool (*EvLoopInvokerTypeEventMonitor)(struct EvLoopInvokerHandle* CPPUTILS_ARG_NN,void*clbkData,void*msg);
 
+// ret<0 is error
+enum EvLoopInvokerLoopReturn {
+    EvLoopInvokerLoopReturnNone = 0,
+    EvLoopInvokerLoopReturnTimeout = 1,
+    EvLoopInvokerLoopReturnQuit = 2
+};
 
-#define EvLoopInvokerCreateHandle()                             EvLoopInvokerCreateHandleEx(CPPUTILS_NULL)
+
+#define EvLoopInvokerCreateHandleForCurThr()                    EvLoopInvokerCreateHandleForCurThrEx(CPPUTILS_NULL)
 #define EvLoopInvokerCallFuncionBlocked(_instance,_fnc,_data)   EvLoopInvokerCallFuncionBlockedEx(_instance,_fnc,_data,CPPUTILS_NULL)
-#define EvLoopInvokerWaitForEventsForever(_instance)            EvLoopInvokerWaitForEventsMs(_instance,-1)
-EVLOOPINVK_EXPORT struct EvLoopInvokerHandle* EvLoopInvokerCreateHandleEx(const void* a_inp) CPPUTILS_NOEXCEPT;
-EVLOOPINVK_EXPORT void  EvLoopInvokerCleanHandle(struct EvLoopInvokerHandle* a_instance) CPPUTILS_NOEXCEPT;
+#define EvLoopInvokerLoopEvLoopThrForever(_instance)            EvLoopInvokerLoopEvLoopThr(_instance,-1)
+EVLOOPINVK_EXPORT struct EvLoopInvokerHandle* EvLoopInvokerCreateHandleForCurThrEx(const void* a_inp) CPPUTILS_NOEXCEPT;
+EVLOOPINVK_EXPORT void  EvLoopInvokerCleanHandleEvLoopThr(struct EvLoopInvokerHandle* a_instance) CPPUTILS_NOEXCEPT;
+EVLOOPINVK_EXPORT void  EvLoopInvokerStopLoopAnyThr(struct EvLoopInvokerHandle* CPPUTILS_ARG_NN a_instance) CPPUTILS_NOEXCEPT;
+EVLOOPINVK_EXPORT int   EvLoopInvokerLoopEvLoopThr(struct EvLoopInvokerHandle* CPPUTILS_ARG_NN a_instance, int64_t a_durationMs) CPPUTILS_NOEXCEPT;
 EVLOOPINVK_EXPORT void* EvLoopInvokerCallFuncionBlockedEx(struct EvLoopInvokerHandle* CPPUTILS_ARG_NN a_instance, EvLoopInvokerBlockedClbk a_fnc, void* a_pData, int* a_pnErrorCode) CPPUTILS_NOEXCEPT;
 EVLOOPINVK_EXPORT int   EvLoopInvokerCallFuncionAsync(struct EvLoopInvokerHandle* CPPUTILS_ARG_NN a_instance, EvLoopInvokerAsyncClbk a_fnc, void* a_pData) CPPUTILS_NOEXCEPT;
 EVLOOPINVK_EXPORT struct EvLoopInvokerEventsMonitor* EvLoopInvokerRegisterEventsMonitorEvLoopThr(struct EvLoopInvokerHandle* CPPUTILS_ARG_NN a_instance, EvLoopInvokerTypeEventMonitor a_fnc, void* a_clbkData) CPPUTILS_NOEXCEPT;
 EVLOOPINVK_EXPORT void EvLoopInvokerUnRegisterEventsMonitorEvLoopThr(struct EvLoopInvokerHandle* CPPUTILS_ARG_NN a_instance, struct EvLoopInvokerEventsMonitor* a_eventsMonitor) CPPUTILS_NOEXCEPT;
-EVLOOPINVK_EXPORT void EvLoopInvokerWaitForEventsMs(struct EvLoopInvokerHandle* CPPUTILS_ARG_NN a_instance, int64_t a_timeMs) CPPUTILS_NOEXCEPT;
 
 CPPUTILS_END_C
 
